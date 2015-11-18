2dd48641188a73e55fa04ef9534a834aa261de0a
g-mac <simonmayrshofer@hotmail.com>
2015-11-18 20:56:00 +0100

    added WebView to show current Web implementation of RefugeeMaps

--- app/src/main/java/de/simonmayrshofer/refugeemaps/WebViewFragment.java
+++ app/src/main/java/de/simonmayrshofer/refugeemaps/WebViewFragment.java
@@ -6,19 +6,46 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 public class WebViewFragment extends Fragment {
 
+    String refugeeMapsUrl = "http://refugeemaps.eu/";
 
     public WebViewFragment() {
     }
 
+    @Bind(R.id.web_view)
+    WebView webView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_web_view, container, false);
+        View view = inflater.inflate(R.layout.fragment_web_view, container, false);
+        ButterKnife.bind(this, view);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setSupportZoom(false);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.loadUrl(refugeeMapsUrl);
+
+        return view;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
 }
