c9de377a50de8afec7f76113fd95dc569c50aed2
g-mac <simonmayrshofer@hotmail.com>
2015-11-19 05:43:22 +0100

    added menu action items to switch between webview(map) and listview(offline)

--- app/src/main/java/de/simonmayrshofer/refugeemaps/MainActivity.java
+++ app/src/main/java/de/simonmayrshofer/refugeemaps/MainActivity.java
@@ -1,8 +1,12 @@
 package de.simonmayrshofer.refugeemaps;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -12,21 +16,54 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.toolbar)
     Toolbar toolbar;
 
+    WebViewFragment webViewFragment;
+    ListViewFragment listViewFragment;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        WebViewFragment webViewFragment = new WebViewFragment();
-        ListViewFragment listViewFragment = new ListViewFragment();
+        webViewFragment = new WebViewFragment();
+        listViewFragment = new ListViewFragment();
 
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.main_activity_content_view, webViewFragment)
-                .commit();
+        replaceFragment(webViewFragment);
+
+//        getSupportFragmentManager()
+//                .beginTransaction()
+//                .add(R.id.main_activity_content_view, webViewFragment)
+//                .commit();
 
         setSupportActionBar(toolbar);
     }
 
+    public void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_activity_content_view, fragment)
+                .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_list:
+                replaceFragment(listViewFragment);
+                return true;
+            case R.id.action_map:
+                replaceFragment(webViewFragment);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
